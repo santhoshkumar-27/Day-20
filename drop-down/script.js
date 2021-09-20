30803fb14a211702be71d084cd24f82025dc9d4a
santhoshkumar-27 <it.santhoshkumar@outlook.com>
2021-09-20 18:53:50 +0530

    changed The classlist

--- drop-down/script.js
+++ drop-down/script.js
@@ -7,8 +7,8 @@ document.addEventListener("click", e => {
         currentDropdown = e.target.closest('[data-dropdown]')
         currentDropdown.classList.toggle('active')
     }
-    document.querySelectorAll("[data-dropdown].active").forEach(dropdown =>{
+    document.querySelectorAll("[data-dropdown].active").forEach(dropdown => { 
         if (dropdown === currentDropdown)return
-        dropdown.classlist.remove('active')
+        dropdown.classList.remove('active')
     })
 })
